0855146954634b414b265d8a8dbbddaa5c90f2c1
taehwankang97 <kwth2002hs@naver.com>
2024-07-22 17:18:44 +0900

    0722 공부한내용

---
+++ 05_oop/src/method/StudentRun.java
@@ -0,0 +1,13 @@
+package method;
+
+// 실행용 클래스
+public class StudentRun {
+
+	public static void main(String[] args) {
+
+		StudentService service = new StudentService();
+		
+		service.mainMenu();
+	}
+
+}
---
+++ 06_object_array/src/student/run/StudentRun.java
@@ -0,0 +1,14 @@
+package student.run;
+
+import student.view.StudentView;
+
+// 실행용
+public class StudentRun {
+
+	public static void main(String[] args) {
+		//studentView 객체생성
+		StudentView view = new StudentView();
+		view.mainMenu();
+	}
+
+}
